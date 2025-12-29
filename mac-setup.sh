f29c3d43fcbeed880492fe543f557d8496844413
Gowtham D <c_gowtham.d@viome.com>
2025-12-29 11:30:21 +0530

    updated keyboard speed

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,2 +1,2 @@
-defaults write -g InitialKeyRepeat -float 10.0 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -float 1.0 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -float 15.0 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -float 2.0 # normal minimum is 2 (30 ms)
